73aaf595ed9243a564143f30de89004ded127411
TamazyanD <davittamazyan1@gmail.com>
2025-12-12 19:55:36 +0400

    Update battery_percentage.sh

--- battery_percentage.sh
+++ battery_percentage.sh
@@ -5,3 +5,7 @@ battery_percentage=$(acpi -b | grep -oP '\d+(?=%)')
 
 #Output of the result
 echo "$battery_percentage"
+
+#ACPI output is very simple, as shown below. 
+#The script just extracts the battery percentage:
+#Battery 0: Full, 92%
